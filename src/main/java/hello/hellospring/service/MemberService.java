3c0bc4b92f6987358e62e8994d8f01929f1ba644
kimsr9210 <kimsr9210@naver.com>
2024-01-19 17:54:38 +0900

    Update MemberService.java

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -11,15 +11,14 @@ import java.util.Optional;
 public class MemberService {
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
-    /**
-     * 회원가입
-     */
+    //회원가입
     @Transactional //JPA는 join들어올때 모든데이터 변경이 트렌젝션 안에서 실행되어야함 
     public Long join(Member member) {
 
@@ -27,14 +26,15 @@ public class MemberService {
         memberRepository.save(member);
         return member.getId();
     }
-
+    //검증 메서드
     public void validateDuplicateMember(Member member){
+        //같은 이름이 있는 중복 회원 x
         memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
+                .ifPresent(m -> { //값이 있으면
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
-
+    //전체회원 조회
     public List<Member> findMembers(){
         return memberRepository.findAll();
     }
